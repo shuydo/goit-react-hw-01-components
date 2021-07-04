aba54c0f11fce589de6cb41447783b2a6c79f3dc
Shuydo <shuydo12@gmail.com>
2021-07-04 16:42:09 +0300

    добавлен условный рендер заголовка в task2

--- src/Statistics/Statistics.jsx
+++ src/Statistics/Statistics.jsx
@@ -1,7 +1,8 @@
 function Statistics({ title, stats }) {
   return (
     <section className="statistics">
-      <h2 className="title">{title}</h2>
+      {title && <h2 className="title">{title}</h2>}
+
       <ul className="stat-list">
         {stats.map(({ id, label, percentage }) => (
           <li className="item" key={id}>
